43225c331f371ad957bc0b3ed443fbf96d4c89c8
sonproject123 <sonproject123@gmail.com>
2025-02-23 11:03:08 +0900

    타일맵 익히기

--- Assets/Scripts/Ground/TileAutoCreate.cs
+++ Assets/Scripts/Ground/TileAutoCreate.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
-public class TileAutoCreate : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+public class TileAutoCreate : MonoBehaviour {
+    [SerializeField] Tilemap tilemap; // 타일맵 컴포넌트
+    [SerializeField] TileBase tilePrefab; // 사용할 타일 프리팹
+
+    private void Start() {
+        CreateTilemap();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void CreateTilemap() {
+        // 타일맵의 크기 설정 (예: 10x10)
+        int width = 10;
+        int height = 10;
+
+        // 타일을 동적으로 추가
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                Vector3Int tilePosition = new Vector3Int(x, y, 0);
+                tilemap.SetTile(tilePosition, tilePrefab);
+            }
+        }
     }
 }
